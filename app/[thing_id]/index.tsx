adcd175c8b1ca86759c31e9bc533cf92160c149c
Mike Hamilton <mike@mikehamilton.ca>
2024-11-07 15:24:38 +0100

    Feat: single screen

--- app/[thing_id]/index.tsx
+++ app/[thing_id]/index.tsx
@@ -1,66 +1,49 @@
 import { Text, ScrollView, View } from "react-native";
 import { Link, useLocalSearchParams } from "expo-router";
+import { ModalWrapper } from "@/components/ModalWrapper";
 
 export default function Index() {
   const { thing_id } = useLocalSearchParams();
 
   return (
-    <View style={{ flex: 1, gap: 40 }}>
-      <Text>This is index for id: {thing_id}</Text>
-      <Link
-        href={{
-          pathname: "/[thing_id]/other",
-          params: {
-            thing_id: thing_id as string,
-          },
-        }}
-      >
-        Other
-      </Link>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Link
-        href={{
-          pathname: "/[thing_id]/other",
-          params: {
-            thing_id: thing_id as string,
-          },
-        }}
-      >
-        Other
-      </Link>
-    </View>
+    <ModalWrapper>
+      <View style={{ flex: 1, gap: 40 }}>
+        <Text>This is index for id: {thing_id}</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+      </View>
+    </ModalWrapper>
   );
 }
